9f81d5e505301c45f2ae2c38b027441801d1a251
Tenieya T Craig <TTCraig01@indianatech.net>
2026-02-04 18:45:38 -0500

    Homework Assignment 1 - Coding Problem

---
+++ CS1300_HW1.py
@@ -0,0 +1,11 @@
+first_name = "Jane"
+last_name = "Doe"
+
+exam_score_1 = 20
+exam_score_2 = 60
+exam_score_3 = 100
+
+average_score = (exam_score_1 + exam_score_2 + exam_score_3) / 3
+
+print(f"Student:", first_name, last_name)
+print(f"Average Score:", average_score)
