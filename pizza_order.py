9717fd2d159cbdcab2805e8073f781521da0e42a
TenieyaC <TTCraig01@indianatech.net>
2026-03-26 14:12:35 -0400

    Improve comments
    Refactor comments for clarity and improve user prompts.

--- pizza_order.py
+++ pizza_order.py
@@ -9,27 +9,22 @@ size_prices = [6.99, 9.99, 12.99, 16.99]
 topping_names = ["Pepperoni", "Mushrooms", "Green Peppers", "Onions", "Sausage", "Bacon", "Extra Cheese", "Pineapple"]
 topping_price = 1.50 # each topping, any size
 
-
 # ----- Order Storage -----
 order_descriptions = [] # e.g., "Large Pepperoni, Mushrooms"
 order_prices = [] # e.g., 15.99
-# Your code goes below this line.
 
 while True:
     size_choice = 0
-    # --- Display size menu (Ex 1) ---
     print ("="*30)
     print ("\tPIZZA SIZES")
     print ("="*30)
 
-    #Loops through and prints the sizes and their price
+    # Loops through the menu when user makes the choice of pizza size.
     for i in range(len(sizes)):
         print(f"{i+1}. {sizes[i]}  \t{size_prices[i]:>5}")
-    #Border
     print ("="*30)
 
-# Invalid Sizes and using Try/Except to allow Intergers to pass and repeat the code.
-    
+# Asks users sizes of pizza and makes sure it's a pizza size 1-4 and not letters.
     try:
         size_choice = int(input("Pick a size (1-4): "))
         if size_choice < 1 or size_choice > 4:
@@ -39,13 +34,12 @@ while True:
         print("Please enter a number!")
         continue
 
-    # Selecting the topics wanted for the pizza and making sure it passes through with the loop
-    # --- Select toppings (Ex 3) ---
+
+    # List of Pizza Toppings after user choice
     for i in range(len(topping_names)):
         print(f"{i+1}. {topping_names[i]}")
 
-    # Topping selection loop (sentinel: "done")
-
+    # Topping selection loop (sentinel: "done" when user exits program)
     selected_toppings = []
     user_input = (input("Enter Topping Number. Type done to finish: "))
     print(user_input)
@@ -56,10 +50,11 @@ while True:
             print("Not a Topping")
         user_input = input("Enter topping number. Type done to finish: ")
 
-    # --- Calculate and store (Ex 4) ---
+    # Calculate prices that combines how many pizzas and toppings prices. - TESTING
     price = size_prices[size_choice - 1] + len(selected_toppings) * topping_price
     print(price)
-    # --- Order another? (Ex 5) ---
+    
+    # Asks if they want another pizza - NEEDS TO KEEP THE PREVIOUS PIZZA AND SO ON
     order_another = input("Order another pizza? (y/n): ")
     if order_another.lower() != "y":
         break
